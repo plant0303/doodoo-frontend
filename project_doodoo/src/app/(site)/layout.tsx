0895be597a75969189d4eaf5841e422a01ebd4bd
seominhee <minheee07@gmail.com>
2025-11-18 21:45:34 +0900

    tailwind 기본 세팅

---
+++ project_doodoo/src/app/(site)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/common/Footer'
+import Header from '@/components/common/Header'
+import React from 'react'
+import "./globals.css";
+import "./globals.scss";
+
+export default function SiteLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
---
+++ project_doodoo/src/app/(site)/list/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      list
+      <div className="w-20 h-20 bg-blue-500"></div>
+    </div>
+  )
+}
+
+export default page
